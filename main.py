a43d14c6bafef70a9da42958c7bdd835317e399c
OSAKAcnCODE <chillgy68@gmail.com>
2026-06-11 22:25:53 +0530

    updated the calculator

--- main.py
+++ main.py
@@ -1,30 +1,16 @@
-class calc:
-    def __init__(self,n1,n2):
-        self.n1=n1
-        self.n2=n2 
-
-class add(calc):
-    def __init__(self, n1, n2):
-        super().__init__(n1, n2)   
+class calc():
+    def __init__(self,num1,num2):
+        self.num1=num1
+        self.num2=num2    
     def add(self):
-        return self.n1+self.n2
-class sub(calc):
-    def __init__(self, n1, n2):
-        super().__init__(n1, n2)   
+        return self.num1+self.num2
     def sub(self):
-        return self.n1-self.n2
-class mul(calc):
-    def __init__(self, n1, n2):
-        super().__init__(n1, n2)   
+        return self.num1-self.num2
     def mul(self):
-        return self.n1*self.n2
-class div(calc):
-    def __init__(self, n1, n2):
-        super().__init__(n1, n2)   
+        return self.num1*self.num2
     def div(self):
-        return self.n1/self.n2
+        return self.num1/self.num2
     
-print(add(15,15))
-mul(15,15)
-sub(15,15)
-div(15,15)
+a=calc(15,15)
+print(a.add())
+    
